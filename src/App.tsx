7f7a0a93e870f7d60dc369f1183f8a165a1f129c
xtrellah <maxestrella565@gmail.com>
2026-03-31 21:08:41 +0100

    Initial commit Initialise react vite ts app Strip out boilerplate code

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import viteLogo from "./assets/vite.svg";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <></>;
+}
+
+export default App;
